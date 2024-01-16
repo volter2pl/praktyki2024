80419ee8c1a506fa20c97809197826643e0f2a77
matlos332 <mateuszszszymczak@gmail.com>
2024-01-16 13:48:21 +0100

    Klikalne Zdj
    Próba zrobienia klikalnego zdjęcia

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import './App.css'
+import witam from './gratest.tsx'
 function App() {
+  const handleClick = () => {
+    console.log('tak');
+  };
+
   return (
     <>
       <header>
@@ -7,23 +12,23 @@ function App() {
       </header>
       <section>
         <div className="container">
-          <img className="minizdj" src="assets/gra1.png"></img>
+          <img className="minizdj" src="src/assets/statek.png" onClick={handleClick} alt="statek"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="assets/gra2.png"></img>
+          <img className="minizdj" src="src/assets/gra2.png" alt="gra2"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="assets/gra3.png"></img>
+          <img className="minizdj" src="src/assets/gra3.png" alt="gra3"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="assets/gra4.png"></img>
+          <img className="minizdj" src="src/assets/gra4.png" alt="gra4"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="assets/gra5.png"></img>
+          <img className="minizdj" src="src/assets/gra5.png" alt="gra5"></img>
         </div>
       </section>
       <main>
-
+      
       </main>
       <footer>
 
---
+++ src/gratest.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function witam() {
+    return (
+        <h1>Witam</h1>
+    );
+}
+
+export default witam;
