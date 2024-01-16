85a74e0486ee025b2a5597648900ecea6179bd5c
Piotr K <75329810+Lobo6780@users.noreply.github.com>
2024-01-16 12:48:38 +0100

    Zmiana ścieżki dostępu do zdjęć

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,16 @@ function App() {
       </header>
       <section>
         <div className="container">
-          <img className="minizdj" src="src/tt.png"></img>
+          <img className="minizdj" src="assets/gra1.png"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="src/tt.png"></img>
+          <img className="minizdj" src="assets/gra2.png"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="src/tt.png"></img>
+          <img className="minizdj" src="assets/gra3.png"></img>
         </div>
         <div className="container">
-          <img className="minizdj" src="src/tt.png"></img>
+          <img className="minizdj" src="assets/gra4.png"></img>
         </div>
       </section>
       <main>
