c225dfb2eee2663abbff9c581afb4d96fa3b0082
Piotr K <75329810+Lobo6780@users.noreply.github.com>
2024-01-17 09:37:18 +0100

    Funkcja wyświetlania
    Naprawione wyświetlanie funkcji po kliknięciu

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,42 @@ import Witam from './gratest.tsx'
 import { useState } from 'react';
 
 function App() {
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState<React.ReactNode>(null);
 
   const handleClick = () => {
-    setMessage('witam');
+    setMessage(<Witam />);
   };
+
+  return (
+    <>
+      <header>
+        <h1 className='Logo'>Gamezz Loader</h1>
+      </header>
+      <section>
+        <div className="container">
+          <img className="minizdj" src="src/assets/statek.png" alt="statek" onClick={handleClick} ></img>
+        </div>
+        <div className="container">
+          {/*<img className="minizdj" src="src/assets/gra2.png" alt="gra2"></img>*/}
+        </div>
+        <div className="container">
+          {/*<img className="minizdj" src="src/assets/gra3.png" alt="gra3"></img>*/}
+        </div>
+        <div className="container">
+          {/*<img className="minizdj" src="src/assets/gra4.png" alt="gra4"></img>*/}
+        </div>
+        <div className="container">
+          {/*<img className="minizdj" src="src/assets/gra5.png" alt="gra5"></img>*/}
+        </div>
+      </section>
+      <main>
+        {message}
+      </main>
+      <footer>
+
+      </footer>
+    </>
+  )
 
   return (
     <>
