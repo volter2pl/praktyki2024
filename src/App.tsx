9d191f9acfdd0f228a03d1d861f824327df7b8d6
Piotr K <75329810+Lobo6780@users.noreply.github.com>
2024-01-17 09:40:05 +0100

    Usunięcie niepotrzebnego kodu

--- src/App.tsx
+++ src/App.tsx
@@ -39,37 +39,6 @@ function App() {
       </footer>
     </>
   )
-
-  return (
-    <>
-      <header>
-        <h1 className='Logo'>Gamezz Loader</h1>
-      </header>
-      <section>
-        <div className="container">
-          <img className="minizdj" src="src/assets/statek.png" alt="statek" onClick={handleClick} ></img>
-        </div>
-        <div className="container">
-          {/*<img className="minizdj" src="src/assets/gra2.png" alt="gra2"></img>*/}
-        </div>
-        <div className="container">
-          {/*<img className="minizdj" src="src/assets/gra3.png" alt="gra3"></img>*/}
-        </div>
-        <div className="container">
-          {/*<img className="minizdj" src="src/assets/gra4.png" alt="gra4"></img>*/}
-        </div>
-        <div className="container">
-          {/*<img className="minizdj" src="src/assets/gra5.png" alt="gra5"></img>*/}
-        </div>
-      </section>
-      <main>
-        {message}
-      </main>
-      <footer>
-
-      </footer>
-    </>
-  )
 }
 
 export default App
