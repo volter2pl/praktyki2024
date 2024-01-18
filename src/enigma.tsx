a3da135cdc2bc9cb3b3b6a8a3ebff63226718a5a
matlos332 <mateuszszszymczak@gmail.com>
2024-01-18 11:13:28 +0100

    cytaty
    dodane cytaty

---
+++ src/enigma.tsx
@@ -0,0 +1,15 @@
+function enigma() {
+    const cytaty = [
+        "Knowing yourself is the beginning of all wisdom.",
+        "It is the mark of an educated mind to be able to entertain a thought without accepting it.",
+        "Patience is bitter, but its fruit is sweet.",
+        "Without friends, no one would want to live, even if he had all other goods.",
+        "It is during our darkest moments that we must focus to see the light.",
+        "Sonata fortepianowa autorstwa: Ludwig van Beethoven",
+        "Jak prętem po jajch",
+    ];
+
+    return cytaty;
+}
+
+export default enigma;
