af2e78c739b318c36821a390ce3d87b32c836bd9
Piotr K <75329810+Lobo6780@users.noreply.github.com>
2024-01-18 11:13:52 +0100

    Podstawowa logika strony

---
+++ src/saper/saper.js
@@ -0,0 +1,7 @@
+// Tworzenie pudełka gameWindow
+const gameWindow = document.createElement("div");
+gameWindow.id = "gameWindow";
+document.body.appendChild(gameWindow);
+
+// Logika gry Saper
+// Twój kod logiki gry Saper tutaj
