f66430cd285a803788449a5126558bda2e3e9017
Blanquefort-Bq <bertrand.bak@gmail.com>
2024-01-18 11:19:29 +0100

    quotes - updated

--- src/enigma.tsx
+++ src/enigma.tsx
@@ -6,7 +6,10 @@ function enigma() {
         "Without friends, no one would want to live, even if he had all other goods.",
         "It is during our darkest moments that we must focus to see the light.",
         "Sonata fortepianowa autorstwa: Ludwig van Beethoven",
-        "Jak prętem po jajch",
+        "Democracy basically means of the people, for the people, by the people. But the people are Retarded",
+        "Hello, there.",
+        "I love democracy.",
+        "Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you"
     ];
 
     return cytaty;
