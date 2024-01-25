ddd99d89ae5593de22822e0c0a4dd844a2567612
Oliwier601 <oliwierwielgosz5@gmail.com>
2024-01-25 11:29:28 +0100

    Naprawa wyświetlania

--- src/Tic_Tac_Toe.tsx
+++ src/Tic_Tac_Toe.tsx
@@ -2,7 +2,7 @@ import './Tic_Tac_Toe/circle.css';
 import { Helmet } from 'react-helmet';
 function Tic_Tac_Toe() {
     return (
-        <div>
+        <div id="home">
             <Helmet><script src='src/Tic_Tac_Toe/circle.js'></script></Helmet>
         </div>
     )
