2398cfdeda32f6d59934a3d33888e7954ab8962a
Blanquefort-Bq <bertrand.bak@gmail.com>
2024-02-01 11:06:50 +0100

    chyba się udalo

--- src/snake/snake.tsx
+++ src/snake/snake.tsx
@@ -2,12 +2,15 @@ import './snake.css';
 import SnakeG from './snake.js'
 
 const Snake = () => {
+    const handleClick = () => {
+        SnakeG();
+      };
     return(
         <div>
             <p id="points">Punkty: 0</p>
             <div className="cialo">
                 <canvas width="400" height="400" id="game"></canvas>
-                {SnakeG()}
+                <button onClick={handleClick}>Graj</button>
             </div>
         </div>
     );
