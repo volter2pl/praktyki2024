2e78a1ae4fcd6d35156783484b3f48570ab6a2c9
Blanquefort-Bq <bertrand.bak@gmail.com>
2024-02-01 11:38:54 +0100

    button color and position changes

--- src/snake/snake.tsx
+++ src/snake/snake.tsx
@@ -4,13 +4,16 @@ import SnakeG from './snake.js'
 const Snake = () => {
     const handleClick = () => {
         SnakeG();
+        const Play = document.querySelector(".StartGame") as HTMLElement;
+        Play.style.display = "none";
+        
       };
     return(
         <div>
             <p id="points">Punkty: 0</p>
             <div className="cialo">
                 <canvas width="400" height="400" id="game"></canvas>
-                <button onClick={handleClick}>Graj</button>
+                <button onClick={handleClick} className='StartGame'>Graj</button>
             </div>
         </div>
     );
