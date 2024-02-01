bfc4faf744312c4cd4132ceb65ac012d75f3322a
Oliwier601 <oliwierwielgosz5@gmail.com>
2024-02-01 13:42:35 +0100

    naprawa

--- src/snake/snake.tsx
+++ src/snake/snake.tsx
@@ -1,16 +1,4 @@
 import './snake.css';
-<<<<<<< HEAD
-import snake from '../snake/snake.js'; 
-import resetGame from '../snake/snake.js'; 
-import React, { useEffect } from 'react';
-
-
-const Snake = () => {
-    useEffect(() => {
-       snake;
-       resetGame();
-      }, []);
-=======
 import SnakeG from './snake.js'
 
 const Snake = () => {
@@ -20,16 +8,12 @@ const Snake = () => {
         Play.style.display = "none";
         
       };
->>>>>>> c27854d05d36395c2e61962084e1ef893bdd4ec5
     return(
         <div>
             <p id="points">Punkty: 0</p>
             <div className="cialo">
                 <canvas width="400" height="400" id="game"></canvas>
-<<<<<<< HEAD
-=======
                 <button onClick={handleClick} className='StartGame'>Play</button>
->>>>>>> c27854d05d36395c2e61962084e1ef893bdd4ec5
             </div>
         </div>
     );
