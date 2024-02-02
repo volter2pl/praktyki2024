84ef444935a2dedfe3c73db78bda7e46e7260eb1
Blanquefort-Bq <bertrand.bak@gmail.com>
2024-02-02 11:36:52 +0100

    dodanie buttonu

---
+++ src/Tic_Tac_Toe/Tic_Tac_Toe.tsx
@@ -0,0 +1,19 @@
+import './Tic_Tac_Toe/circle.css';
+import TTTG from'./circle.js'
+
+
+function Tic_Tac_Toe() {
+    const handleClick = () => {
+        TTTG();
+        const Play = document.querySelector(".StartGame") as HTMLElement;
+        Play.style.display = "none";
+        
+      };
+    return (
+        <div id="home">
+            <button onClick={handleClick} className='StartGame'>Play</button>
+        </div>
+    )
+
+}
+export default Tic_Tac_Toe;
